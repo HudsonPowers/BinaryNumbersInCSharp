98bdad6697fcc29bd6335d3a15d448d69e0cdddc
HudsonPowers <89892390+HudsonPowers@users.noreply.github.com>
2022-01-12 12:12:02 -0800

    Set up

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryNumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
